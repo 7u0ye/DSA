5d0937d11901d6f625a8fbcc8cf814591141166f
qinyuan <2229168425@qq.com>
2026-03-14 16:13:42 +0800

    斐波那契

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
-        }
+        System.out.println(fib2(1));
     }
+
+    //斐波那契
+    public static int fib1 (int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fib1(n-1) + fib1(n-2);
+    }
+    public static int fib2 (int n) {
+        int first = 0;
+        int second = 1;
+        for (int i = 0; i < n-1; i++) {
+            int temp = second;
+            second = first + second;
+            first = temp;
+        }
+        return second;
+    }
+
+
 }
